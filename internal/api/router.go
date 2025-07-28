285576e26356125178f84aa7c89274756efa4f7e
taekwondodev <taekwondodev@outlook.it>
2025-07-28 22:27:58 +0200

    update router

--- internal/api/router.go
+++ internal/api/router.go
@@ -7,18 +7,30 @@ import (
 	"github.com/taekwondodev/go-PassKey-Authentication/internal/middleware"
 )
 
-var router *http.ServeMux
-
-func SetupRoutes(authController controller.AuthController) *http.ServeMux {
-	router = http.NewServeMux()
-	setupPreflightRoute()
-	setupRegisterRoutes(authController)
-	setupLoginRoutes(authController)
-	setupTokenRoutes(authController)
-	return router
+type Router struct {
+	mux *http.ServeMux
 }
 
-func applyMiddleware(h middleware.HandlerFunc) http.HandlerFunc {
+func NewRouter() *Router {
+	return &Router{
+		mux: http.NewServeMux(),
+	}
+}
+
+func SetupRoutes(authController controller.AuthController) *http.ServeMux {
+	router := NewRouter()
+	return router.setupRoutes(authController)
+}
+
+func (r *Router) setupRoutes(authController controller.AuthController) *http.ServeMux {
+	r.setupPreflightRoute()
+	r.setupRegisterRoutes(authController)
+	r.setupLoginRoutes(authController)
+	r.setupTokenRoutes(authController)
+	return r.mux
+}
+
+func (r *Router) applyMiddleware(h middleware.HandlerFunc) http.HandlerFunc {
 	return middleware.CorsMiddleware(
 		middleware.ErrorHandler(
 			middleware.LoggingMiddleware(h),
@@ -26,22 +38,22 @@ func applyMiddleware(h middleware.HandlerFunc) http.HandlerFunc {
 	)
 }
 
-func setupPreflightRoute() {
-	router.Handle("OPTIONS /", middleware.CorsMiddleware(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+func (r *Router) setupPreflightRoute() {
+	r.mux.Handle("OPTIONS /", middleware.CorsMiddleware(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusNoContent)
 	})))
 }
 
-func setupRegisterRoutes(authController controller.AuthController) {
-	router.Handle("POST /register/begin", applyMiddleware(authController.BeginRegister))
-	router.Handle("POST /register/finish", applyMiddleware(authController.FinishRegister))
+func (r *Router) setupRegisterRoutes(authController controller.AuthController) {
+	r.mux.Handle("POST /register/begin", r.applyMiddleware(authController.BeginRegister))
+	r.mux.Handle("POST /register/finish", r.applyMiddleware(authController.FinishRegister))
 }
 
-func setupLoginRoutes(authController controller.AuthController) {
-	router.Handle("POST /login/begin", applyMiddleware(authController.BeginLogin))
-	router.Handle("POST /login/finish", applyMiddleware(authController.FinishLogin))
+func (r *Router) setupLoginRoutes(authController controller.AuthController) {
+	r.mux.Handle("POST /login/begin", r.applyMiddleware(authController.BeginLogin))
+	r.mux.Handle("POST /login/finish", r.applyMiddleware(authController.FinishLogin))
 }
 
-func setupTokenRoutes(authController controller.AuthController) {
-	router.Handle("POST /refresh", applyMiddleware(authController.Refresh))
+func (r *Router) setupTokenRoutes(authController controller.AuthController) {
+	r.mux.Handle("POST /refresh", r.applyMiddleware(authController.Refresh))
 }
